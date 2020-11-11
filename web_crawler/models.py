e706a21c1af9b09ba18ed2170425f98334828288
jalali23 <s.jalalifath23@gmail.com>
2020-11-10 22:39:44 -0800

    import data

--- web_crawler/models.py
+++ web_crawler/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Courses(models.Model):
     title=models.CharField(max_length=200)
-    teacher=models.CharField(max_length=200)
+    teacher=models.CharField(max_length=200,blank=True,null=True)
     price=models.CharField(max_length=200,blank=True,null=True)
     category = models.CharField(max_length=128)
     time=models.CharField(max_length=200,blank=True,null=True)
--- web_crawler/views.py
+++ web_crawler/views.py
@@ -5,7 +5,7 @@ import json
 
 def importer(request):
     i = 0
-    f = open("./data/zaban.json")
+    f = open("./data/modirsabz_courses.json")
     a = json.load(f)
     f.close()
     for item in a:
@@ -13,12 +13,30 @@ def importer(request):
             i = 1
             continue
         obj = Courses(
-            teacher = item["teacher"],
+           # teacher = item["teacher"],
+           # time=item["time"][0],
             price = item["price"][0],
             image = item["image"][0],
             title = item["title"][0],
-            category = "مهارت های زبان",
+            category = "مدیریت و بازاریابی",
             Url= item["url"],
         )
         obj.save()
     return HttpResponse("Done!")
+# def shower(request):
+#     data_list=[]
+#     data_str=''
+#     data_list=set(data_list)
+#     data=Courses.objects.all()
+#     for i in data:
+#         if i.category not in data_list:
+#             data_list.add(i.category+'--------\n')
+#     for i in data_list:
+#         data_str += str(i)+ "\n\t"
+#     print(data_str)
+#     return HttpResponse(data_list)
+# def deleter(request):
+#     query=Courses.objects.filter(category='دوره های رایگان')
+#     for i in query:
+#         i.delete()
+#     return HttpResponse(query)
