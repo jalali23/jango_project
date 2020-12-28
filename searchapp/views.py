e77352db5d99330b343fdca00b88b260b9a1cc27
mohammadhosein <mohamadhos88@gmail.com>
2020-12-28 08:01:08 +0330

    beggining works for search page

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from web_crawler.models import * 
 
 def results(request):
-    return render(request,'result.html')
+    from django.db.models import Q
+    if request.method == "GET":
+        query = request.GET.get('q')
+        courses = []
+        if query:
+            queryset = Q(title__icontains=query)
+            courses = Courses.objects.filter(query).distinct()
+        context = {
+            "courses" : courses ,
+        }
+    return render(request,'result.html',context)
 
 def search(request):
     return render(request, 'search.html')
