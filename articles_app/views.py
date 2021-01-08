9e292be08aac79c4eafd1fb58682d5074296c3e8
mohammadhosein <mohamadhos88@gmail.com>
2021-01-08 21:14:18 +0330

    articles search backend

--- articles_app/views.py
+++ articles_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from web_crawler.models import Articles
 # Create your views here.
 def articles(request):
-    return render(request,'articles.html')
+    from django.db.models import Q
+    if request.method == "GET":
+        query = request.GET.get('q')
+        articles = Articles.objects.all()
+        if query:
+            queryset = Q(title__icontains=query)
+            articles = Articles.objects.filter(queryset).distinct()
+        context = {
+            "articles" : articles ,
+        }
+    return render(request,'articles.html',context)
