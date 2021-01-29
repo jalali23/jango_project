cf1158d9f3fb0a87f003d78ea887159b7768f008
mohammadhosein <mohamadhos88@gmail.com>
2021-01-29 03:33:03 +0330

    changes for course prices on backend + make the title dynamic

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from web_crawler.models import *
+from web_crawler.models import Courses
 
 def results(request):
     from django.core.paginator import Paginator
@@ -9,20 +9,24 @@ def results(request):
         category = request.GET.get('category')
         page = request.GET.get('page')
         courses = Courses.objects.all()
+        price = None
+        
+        if category:
+            courses = Courses.objects.filter(Q(category__icontains=category)).distinct()
+        elif query:
+            courses = Courses.objects.filter(Q(title__icontains=query)).distinct() 
+        else:
+            courses = Courses.objects.all()
 
-        if query:
-            queryset = Q(title__icontains=query)
-            courses = Courses.objects.filter(queryset).distinct()
-
-        elif category:
-            categoryQuery = Q(category__icontains=category)
-            courses = Courses.objects.filter(categoryQuery).distinct()
+        if price:
+            courses = courses.filter(price__range=(min_price, max_price))
+        
         paginator = Paginator(courses, 10)
         courses = paginator.get_page(page)
         context = {
             "courses" : courses ,
             "category" : category,
-            "query" : query,
+            "price" : price
         }
     return render(request,'result.html',context)
 
--- web_crawler/models.py
+++ web_crawler/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Courses(models.Model):
     title=models.CharField(max_length=200,null=True,blank=True)
     teacher=models.CharField(max_length=200,blank=True,null=True)
